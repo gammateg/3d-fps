98f408e03194ea760670dc63b68e63b1f37c5180
gammateg <uystids@gmail.com>
2025-11-27 00:11:54 +0300

    fps controller

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,19 +3,24 @@ extends CharacterBody3D
 @export var speed = 10
 @export var sens = 0.003
 @export var gravity = 9.8
-@export var jump_force = 100.0
+@export var jump_force = 7.0
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
-	velocity.y -= gravity
-	var dir = Input.get_vector("left","right","back","front")
+	var input_dir = Input.get_vector("left","right","back","front")
+	var dir = transform.basis * Vector3(input_dir.x, 0, -input_dir.y).normalized() * speed
 	
-	velocity = transform.basis * Vector3(dir.x, velocity.y, -dir.y).normalized() * speed
+	velocity.x = dir.x
+	velocity.z = dir.z
+	
+	velocity.y -= gravity * delta
 	
 	if (Input.is_action_just_pressed("jump") and is_on_floor()):
 		velocity.y = jump_force
+	
+	print(velocity)
 	
 	move_and_slide()
 
