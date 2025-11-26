b26904f407c06b953981c4393191c145d64bbdf3
gammateg <uystids@gmail.com>
2025-11-26 14:55:25 +0300

    bk

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,16 @@
 extends CharacterBody3D
 
 @export var speed = 100
-@export var sens = 0.01
+@export var sens = 0.003
 
 func _ready() -> void:
-	pass
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
 	pass
 
 func _input(event: InputEvent) -> void:
-	if (event is InputEventMouse):
-		print(event.relative)
+	if (event is InputEventMouseMotion):
+		rotation.y -= event.relative.x * sens
+		$Camera3D.rotation.x = clamp($Camera3D.rotation.x-event.relative.y * sens, -1.6, 1.6)
+		print($Camera3D.rotation.x)
